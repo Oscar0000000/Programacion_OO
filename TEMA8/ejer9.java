6ee3fca116e7a26660656000d78efaf12dd912e1
Oscar0000000 <oscaramcort13@gmail.com>
2026-04-20 12:29:21 +0200

    Update ejer9.java

--- TEMA8/ejer9.java
+++ TEMA8/ejer9.java
@@ -11,20 +11,20 @@ public class ejercicio9 {
 		
 		SortedSet<Persona> personas = new TreeSet<>();
 		// SortedSet no admite duplicados, TreeSet ordena en orden natural
-		Persona jacobo = new Persona("Jacobo");
+		Persona paco = new Persona("Paco");
 		personas.add(jacobo);
-		Persona simon = new Persona("Simon");
+		Persona mario = new Persona("Mario");
 		personas.add(simon);
 		Persona maria = new Persona("Maria");
 		personas.add(maria);
-		Persona simon2 = new Persona("Simon");
-		personas.add(simon2);
+		Persona mario2 = new Persona("Mario");
+		personas.add(mario2);
 		
 		
 		System.out.println(personas);
 		
-		System.out.println(jacobo.compareTo(simon));// Devuelve numero en negativo porque simon es más grande que jacobo
-		System.out.println(simon.compareTo(simon2));// Devuelve 0 porque son iguales
+		System.out.println(jacobo.compareTo(mario));// Devuelve numero en negativo porque simon es más grande que jacobo
+		System.out.println(simon.compareTo(mario2));// Devuelve 0 porque son iguales
 	}
 
 }
