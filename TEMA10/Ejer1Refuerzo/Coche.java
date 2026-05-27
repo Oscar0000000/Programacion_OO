a7e0bda14f7b3713591c752bce236e02ac40cc5f
Oscar0000000 <oscaramcort13@gmail.com>
2026-05-27 19:42:05 +0200

    Create Coche.java

---
+++ TEMA10/Ejer1Refuerzo/Coche.java
@@ -0,0 +1,47 @@
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Coche implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Esta es la clave primaria que se auto incrementea
+    
+    private String color; // Lo que nos pide de tipo texto
+
+    // Constructor sin parámetros 
+    public Coche() {
+    }
+
+    // Constructor con parametro pa que sea mas facil crear la instancia
+    public Coche(String color) {
+        this.color = color;
+    }
+
+    // Métodos Getter y Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [id=" + id + ", color=" + color + "]";
+    }
+}
